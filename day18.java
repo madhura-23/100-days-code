331bc827d3ec49912de9063517d4f01c7a475ec1
Madhura Bhat <madhurabhat2310@gmail.com>
2025-08-02 21:39:48 +0530

    Add toLowerCase method implementation
    Introduced day18.java with a custom toLowerCase method that converts uppercase letters in a string to lowercase without using built-in functions.

---
+++ day18.java
@@ -0,0 +1,15 @@
+public class day18{
+    public String toLowerCase(String s) {
+            String str="";
+        for(int i= 0 ; i<s.length();i++){
+            if(s.charAt(i)>='A'&& s.charAt(i)<='Z'){
+                str+=(char)(s.charAt(i)+32);
+
+            }
+            else{
+                str+=s.charAt(i);
+            }
+        }
+        return str;
+    }
+}
