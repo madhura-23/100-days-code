e4986007f2108289c75d69dd6c57e30c5523c026
Madhura Bhat <madhurabhat2310@gmail.com>
2025-08-08 21:22:47 +0530

    Add isAnagram method to day24.java
    Introduces a method to check if two strings are anagrams by sorting and comparing their character arrays.

---
+++ day24.java
@@ -0,0 +1,18 @@
+import java.util.Arrays;
+
+public class day24 {
+    public boolean isAnagram(String s, String t) {
+        if (s.length() == t.length()) {
+            char[] x = s.toCharArray();
+            char[] y = t.toCharArray();
+
+            Arrays.sort(x);
+            Arrays.sort(y);
+
+            return Arrays.equals(x, y);
+        } else {
+            return false;
+        }
+    }
+}
+
