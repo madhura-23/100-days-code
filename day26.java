04b61b85107e0974a09ae0bf853c096fc0fa86c4
Madhura Bhat <madhurabhat2310@gmail.com>
2025-08-10 19:12:16 +0530

    Add maxArea solution for container problem
    Introduced day26.java with an implementation of the two-pointer approach to solve the container with most water problem.

---
+++ day26.java
@@ -0,0 +1,18 @@
+public class day26{
+    public int maxArea(int[] height) {
+        int left =0; 
+        int right=height.length-1;
+        int maxArea =0;
+        while(left<right){
+            int currentArea =Math.min(height[left], height[right]) *(right -left);
+            maxArea =Math.max(maxArea, currentArea);
+            if(height[left]<height[right]){
+                left++;
+            }
+            else{
+                right--;
+            }
+        }
+        return maxArea;
+    }
+}
