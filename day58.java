e7c0fb83e0ce856f1339b678f08d2bd35ffba54a
madhura-23 <madhurabhat2310@gmail.com>
2025-09-11 21:22:00 +0530

    Add canVisitAllRooms solution using DFS
    Implements a method to determine if all rooms can be visited given a list of keys in each room. Uses depth-first search to traverse rooms and checks if all have been visited.

---
+++ day58.java
@@ -0,0 +1,27 @@
+import java.util.List;
+public class day58{
+    class Solution {
+    public boolean canVisitAllRooms(List<List<Integer>> rooms) {
+        boolean[] visited = new boolean[rooms.size()];
+        dfs(0, rooms, visited);
+
+        // Check if all rooms were visited
+        for (boolean roomVisited : visited) {
+            if (!roomVisited) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    private void dfs(int room, List<List<Integer>> rooms, boolean[] visited) {
+        visited[room] = true;
+        for (int key : rooms.get(room)) {
+            if (!visited[key]) {
+                dfs(key, rooms, visited);
+            }
+        }
+    }
+}
+
+}
