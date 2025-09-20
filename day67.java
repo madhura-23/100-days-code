851784e73915d415fde34aaffdf51f949dcc8af3
madhura-23 <madhurabhat2310@gmail.com>
2025-09-20 22:05:35 +0530

    Add solution for finding single number using XOR
    Introduced day67.java with a Solution class that finds the element appearing only once in an array where every other element appears twice, using XOR operation.

---
+++ day67.java
@@ -0,0 +1,11 @@
+public class day67{
+    class Solution {
+    public int singleNumber(int[] nums) {
+        int xor=0;
+        for(int i=0;i<nums.length;i++){
+            xor=xor^nums[i];
+        }
+        return xor;
+    }
+}
+}
