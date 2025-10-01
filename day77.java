2af2c0ea9d011d976ba84a2f15a983abdc7ddadf
madhura-23 <madhurabhat2310@gmail.com>
2025-10-01 20:10:03 +0530

    Add solution for water bottles exchange problem
    Implements a method to calculate the total number of water bottles that can be drunk given an initial number of bottles and an exchange rate for empty bottles.

---
+++ day77.java
@@ -0,0 +1,17 @@
+public class day77 {
+    class Solution {
+    public int numWaterBottles(int numBottles, int numExchange) {
+        int drinked = 0;
+        int eb = 0; // empty bottles
+        
+        while (numBottles > 0) {
+            drinked += numBottles;   // drink all current bottles
+            eb += numBottles;        // get empty bottles
+            
+            numBottles = eb / numExchange; // exchange empties for new bottles
+            eb = eb % numExchange;         // leftover empties
+        }
+        return drinked;
+    }
+}
+}
