7bb645c3007161f7d486f7a6a45e12aaaed65004
madhura-23 <madhurabhat2310@gmail.com>
2025-10-02 20:56:09 +0530

    Add solution for maxBottlesDrunk problem
    Implements a method to calculate the maximum number of bottles that can be drunk given an initial number of bottles and an increasing exchange rate for empty bottles.

---
+++ day78.java
@@ -0,0 +1,16 @@
+public class day78{
+    class Solution {
+    public int maxBottlesDrunk(int numBottles, int numExchange) {
+        int bottleDrunk = numBottles;
+        int emptyBottles = numBottles;
+
+        while (emptyBottles >= numExchange) {
+            emptyBottles -= numExchange; // use empties for exchange
+            numExchange++;               // cost of exchange increases
+            bottleDrunk++;               // got one more bottle
+            emptyBottles++;              // the new bottle becomes empty
+        }
+        return bottleDrunk;
+    }
+}
+}
