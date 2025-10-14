8dc4c218b94a849ae1222de59ae576f68adb624c
madhura-23 <madhurabhat2310@gmail.com>
2025-10-14 18:39:09 +0530

    Add day89 solution and update List class
    Added day89.java with a solution to check for increasing subarrays. Updated List.java to include stub methods for size() and get(). Also imported Arrays in day84.java.

--- List.java
+++ List.java
@@ -1,4 +1,14 @@
 
 public class List<T> {
 
+    public int size() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'size'");
+    }
+
+    public int get(int i) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'get'");
+    }
+
 }
