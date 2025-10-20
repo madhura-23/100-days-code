9afb4a77e89fb9ee556cbaca1fb813d2a1cb1a1d
madhura-23 <madhurabhat2310@gmail.com>
2025-10-20 19:21:05 +0530

    Final Value of Variable After Performing Operations

---
+++ day97.java
@@ -0,0 +1,7 @@
+puvlic class day97{
+    class Solution {
+    public int finalValueAfterOperations(String[] operations) {
+        return java.util.Arrays.stream(operations).mapToInt(op -> op.charAt(1) == '+' ? 1 : -1).sum();
+    }
+}
+}
