2e6326105795b9aa065ab22b499b5250716a9a98
madhura-23 <madhurabhat2310@gmail.com>
2025-10-21 23:01:25 +0530

    Fix typo in day97 and add uniquePaths solution in day98
    Corrected 'puvlic' to 'public' in day97.java. Added day98.java with a Solution class implementing the uniquePaths algorithm for grid traversal.

--- day97.java
+++ day97.java
@@ -1,4 +1,4 @@
-puvlic class day97{
+public class day97{
     class Solution {
     public int finalValueAfterOperations(String[] operations) {
         return java.util.Arrays.stream(operations).mapToInt(op -> op.charAt(1) == '+' ? 1 : -1).sum();
